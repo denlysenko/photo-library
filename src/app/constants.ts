8ae296e22742a2ef11e7755bd141564d09b0cd17
denlysenko <den.lysenko.mail@gmail.com>
2020-07-10 19:33:05 +0300

    add constants file

---
+++ src/app/constants.ts
@@ -0,0 +1,12 @@
+export const FAVORITES_KEY = 'favorites_photos';
+
+export const menuItems = [
+  {
+    link: '/',
+    label: 'Photos',
+  },
+  {
+    link: '/favorites',
+    label: 'Favorites',
+  },
+];
