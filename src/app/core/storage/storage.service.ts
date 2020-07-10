f0871492c2f3c92cc0086e2847cc36f1241dd46a
denlysenko <den.lysenko.mail@gmail.com>
2020-07-10 19:31:53 +0300

    move data serialization into storage service

--- src/app/core/storage/storage.service.ts
+++ src/app/core/storage/storage.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class StorageService {
-  getItem(key: string): string {
-    return localStorage.getItem(key);
+  getItem<T>(key: string): T {
+    return JSON.parse(localStorage.getItem(key));
   }
 
-  setItem(key: string, value: string): void {
-    localStorage.setItem(key, value);
+  setItem<T>(key: string, value: T): void {
+    localStorage.setItem(key, JSON.stringify(value));
   }
 
   removeItem(key: string): void {
